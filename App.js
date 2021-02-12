d95ce27f0883dec9218967a2a2a3f68d704ced19
Banannabomb <36425629+Banannabomb@users.noreply.github.com>
2021-02-11 21:56:11 -0800

    New Branch and added all the files needed to run Expo

---
+++ App.js
@@ -0,0 +1,8 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Detect from "./App/Screens/Detect";
+
+export default function App() {
+	return <Detect />;
+}
