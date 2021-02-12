7f2f06a53c76d15f5352a2b14d41170d92eafe93
CagedRage <krish.katariya@hotmail.com>
2021-02-12 11:09:50 -0800

    Updated touchable opacity for detect.js

--- Detect.js
+++ Detect.js
@@ -1,57 +1,52 @@
-import React from "react";
-import {
-	ImageBackground,
-	SafeAreaView,
-	StyleSheet,
-	Text,
-	View,
-	TouchableOpacity,
-	Dimensions,
-} from "react-native";
+import React from 'react';
+import { ImageBackground, SafeAreaView, StyleSheet, Text, View, TouchableOpacity, Dimensions } from 'react-native';
 // Get the image through url (imgur)
-const image = { uri: "https://i.imgur.com/NLwCJeA.png" };
-const deteectimage = { uri: "https://i.imgur.com/PhHulFx.png" };
+const image = { uri : "https://i.imgur.com/NLwCJeA.png"};
+const deteectimage = {uri: "https://i.imgur.com/ntJM5VX.png"}; 
 function Detect(props) {
-	return (
-		//Safe area view for Iphone's, contains all the information
+    return (
+        //Safe area view for Iphone's, contains all the information
 
-		<View style={styles.container}>
-			<ImageBackground source={image} style={styles.image}>
-				<TouchableOpacity onPress={() => alert("Button pressed")}>
-					<ImageBackground
-						source={deteectimage}
-						style={styles.detectStyle}
-					></ImageBackground>
-				</TouchableOpacity>
-			</ImageBackground>
-		</View>
-	);
+        <View style = {styles.container}> 
+            <ImageBackground source = {image} style = {styles.image}>
+                <TouchableOpacity style = {styles.button} onPress={() => alert("Button pressed")}>
+                    <ImageBackground source={deteectimage} style= {styles.imageD}>
+                    </ImageBackground>
+                </TouchableOpacity>
+            </ImageBackground>
+        </View>
+    );
 }
-const win = Dimensions.get("window");
+const win = Dimensions.get('window');
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-	},
-	image: {
-		flex: 1,
-		resizeMode: "cover",
-		justifyContent: "center",
-	},
-	text: {
-		color: "white",
-		fontSize: 42,
-		fontWeight: "bold",
-		textAlign: "center",
-		backgroundColor: "#000000a0",
-	},
-	title: {
-		color: "white",
-		fontSize: 24,
-		padding: 20,
-	},
-	detectStyle: {
-		aspectRatio: 1,
-		resizeMode: "contain",
-	},
+    container: {
+        flex: 1,
+    },
+    image: {
+        flex: 1,
+        justifyContent: "center",
+        resizeMode: "contain"
+    },
+    text: {
+        color: "white",
+        fontSize: 42,
+        fontWeight: "bold",
+        textAlign: "center",
+        backgroundColor: "#000000a0"
+    },
+    title: {
+        color: "white",
+        fontSize: 24,
+        padding: 20,
+    },
+    button: {
+        padding: 50
+    },
+    imageD: {
+
+        aspectRatio: 1, 
+        alignItems: "center"
+
+    },
 });
 export default Detect;
