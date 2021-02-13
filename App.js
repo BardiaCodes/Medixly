6be9a1fb15effb30f8c1d572ce34f7589a1f5c8c
AB027897 <47727880+AB027897@users.noreply.github.com>
2021-02-13 18:14:44 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Text, View, StyleSheet, Image } from 'react-native';
-import AssetExample from './components/AssetExample';
+import HomeScreen from './HomeScreen';
 export default function App() {
   return <HomeScreen/>;
 }
