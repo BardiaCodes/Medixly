e597ad3d8fc6173bc594829f54513dd6c8b586b8
AB027897 <47727880+AB027897@users.noreply.github.com>
2021-02-12 19:38:11 -0500

    Add files via upload
    Home Screen Files

---
+++ App.js
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, Image } from 'react-native';
+import AssetExample from './components/AssetExample';
+export default function App() {
+  return <AssetExample/>;
+}
+
