249e7c964ffd407b1d7e26e6eb9974acc57ec1d8
MajesticAxis <53668449+sidgithub780@users.noreply.github.com>
2021-02-13 19:57:01 -0800

    pastresults screen front end finished

--- PastResults.js
+++ PastResults.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { ImageBackground, SafeAreaView, StyleSheet, Text, View, TouchableOpacity, Dimensions, Image } from 'react-native';
+import PastResultsBar from "./components/PastResultsBar";
 
 const button_BACK = { uri: "https://imgur.com/2zC4NGP.png" } // Back button
 const bar_FILL = { uri: "https://imgur.com/Y7IFm2h.png" } // Texture for filled part of bar
@@ -16,15 +17,28 @@ const TestScreen = () => {
 
       <ImageBackground source={BG} style={styles.image}>
 
-        <TouchableOpacity   
-          onPress={() => console.log(win.height)} 
-          style={styles.TOstyle}
-        >
-          <Image 
-            source = { button_BACK }
-            style = {styles.backButton}
-          />
-        </TouchableOpacity>
+        <View style={styles.backView}>
+
+          <TouchableOpacity   
+            onPress={ () => alert("GOOOOOIN BACK!") } 
+            style={styles.TOstyle}
+          >
+            <Image 
+              source = { button_BACK }
+              style = {styles.backButton}
+            />
+
+          </TouchableOpacity>
+
+        </View>
+
+        <View style={styles.barView}>
+
+          <PastResultsBar />
+          <PastResultsBar />
+          <PastResultsBar />
+
+        </View>
 
       </ImageBackground>
 
@@ -36,12 +50,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: "column",
-    justifyContent: "flex-start"
+    justifyContent: "flex-end"
   },
   image: {
     flex: 1,
     resizeMode: "cover",
-    justifyContent: "center"
+    justifyContent: "flex-start",
+    flexDirection: "column"
   },
   text: {
     color: "white",
@@ -51,13 +66,23 @@ const styles = StyleSheet.create({
     backgroundColor: "black"
   },
   backButton: {
-    width: 100,
-    height: 50,
+    width: 110,
+    height: 65,
     position: "absolute"
   },
   TOstyle: {
-    top: -390, 
-    right: 5
+    margin: 10
+  },
+  backView: {
+    justifyContent: "flex-start",
+    flex: 1
+  },
+  barView: {
+    justifyContent: "center",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 50
   }
 });
 
