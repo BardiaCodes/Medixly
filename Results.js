7a27708ef25f4259f458a11c7be7bb9e3855722b
Banannabomb <36425629+Banannabomb@users.noreply.github.com>
2021-02-26 10:22:59 -0800

    Added backbutton.js component for backbuton

--- Results.js
+++ Results.js
@@ -9,6 +9,7 @@ import {
 	Dimensions,
 	Image,
 } from "react-native";
+import Arrow from "./App/Components/BackButton";
 // Get the image through url (imgur)
 const aPercent = 50; // Asymetry percentage
 const bPercent = 20; // Border percentage
@@ -19,11 +20,15 @@ function Results(props) {
 	return (
 		//Safe area view for Iphone's, contains all the information
 		<ImageBackground source={BG} style={styles.image}>
-		<View style={{zIndex: 999}}>
-				<View style={styles.backButton}>
-					<Arrow thickness={6} size={10} width={40} height={25} left={27} top={10} color="#418DFF"></Arrow>
-				</View>
-			</View>
+			<Arrow
+				thickness={6}
+				size={10}
+				width={40}
+				height={25}
+				left={27}
+				top={10}
+				color="#418DFF"
+			></Arrow>
 			<View style={styles.barBackground}>
 				<View style={styles.barFill} width={125 + 2.29 * aPercent}></View>
 				<View style={styles.barImage}>
@@ -87,17 +92,6 @@ const styles = StyleSheet.create({
 		aspectRatio: 1,
 		resizeMode: "contain",
 	},
-	backButton: {
-		backgroundColor: "white",
-		borderColor: "white",
-		height: 60,
-		width: 125,
-		borderWidth: 1,
-		borderRadius: 200,
-		bottom: 10,
-		left: 20,
-		position: 'absolute'
-	},
 	barBackground: {
 		backgroundColor: "#EDEDED",
 		marginRight: 30,
@@ -127,58 +121,5 @@ const styles = StyleSheet.create({
 		borderRadius: 200,
 	},
 });
-
-// Code for back button arrow
-const Arrow = ({thickness, color, size, width, height, left, top}) => {
-  const arrowWidth = thickness;
-  const styles = StyleSheet.create({
-    container: {
-      backgroundColor: "transparent",
-      overflow: "visible",
-	  left: left,
-	  top: top,
-      width: width * 1.9,
-      height: height * 1.6
-    },
-    arrowTail: {
-      backgroundColor: color,
-      width: width * 1.3,
-      height: arrowWidth,
-      left: width * 0.1,
-	  borderTopColor: "transparent",
-      borderStyle: "solid",
-      position: "absolute",
-	  top: height * 0.65,
-    },
-    arrowHead2: {
-      backgroundColor: color,
-      width: height * 0.9,
-      height: arrowWidth,
-      borderTopColor: "transparent",
-      borderStyle: "solid",
-      transform: [{ rotate: "45deg" }],
-      position: "absolute",
-      top: height * 0.9
-    },
-    arrowHead1: {
-      backgroundColor: color,
-      width: height * 0.9,
-      height: arrowWidth,
-      borderTopColor: "transparent",
-      borderStyle: "solid",
-      transform: [{ rotate: "135deg" }],
-      position: "absolute",
-      top: height * 0.4
-    }
-  });
-
-  return (
-    <View style={styles.container}>
-      <View style={styles.arrowHead1} />
-      <View style={styles.arrowHead2} />
-      <View style={styles.arrowTail} />
-    </View>
-  );
-};
 
 export default Results;
