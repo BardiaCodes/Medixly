17a9cb8064dcb66b78fd9d602932fce9080dbc9a
CagedRage <krish.katariya@hotmail.com>
2021-03-11 21:13:06 -0800

    Added in progresss firebase functionality

--- App.js
+++ App.js
@@ -3,7 +3,13 @@ import { createStackNavigator } from "react-navigation-stack";
 import Detect from "./Detect";
 import Results from "./Results";
 import PastResults from "./PastResults";
+import * as firebase from 'firebase';
+import {firebaseConfig} from "./config"
+import React from "react";
+import * as Google from 'expo-google-app-auth';
+import { Button } from "react-native";
 
+firebase.initializeApp(firebaseConfig);
 const navigator = createStackNavigator(
 	{
 		Detect: Detect,
@@ -17,5 +23,61 @@ const navigator = createStackNavigator(
 		},
 	}
 );
+const AppNavigator = createAppContainer(navigator);
 
-export default createAppContainer(navigator);
+class LoadingScreen extends Component {
+	
+
+	signInWithGoogleAsync = async () => {
+		try {
+			const result = await Google.logInAsync({
+			
+				//androidClientId: YOUR_CLIENT_ID_HERE,
+				behavior: "web",
+				iosClientId: '823887287984-b4s258gu72inqefe5mplcessk492364u.apps.googleusercontent.com',
+				scopes: ['profile', 'email'],
+			});
+
+			if (result.type === 'success') {
+				return result.accessToken;
+			} else {
+				return { cancelled: true };
+			}
+		} catch (e) {
+				return { error: true };
+		}
+	}
+
+	componentDidMount() {
+		this.checkIfLoggedIn();
+	}
+
+	checkIfLoggedIn = () => {
+		firebase.auth().onAuthStateChanged(
+			function(user) {
+				if(user)
+				{
+					this.props.navigation.navigate('DetectScreen');
+				} else {
+					this.props.navigation.navigate('LoginScreen');
+				}
+			}.bind(this)
+		);
+	}
+	render() {
+		return (
+			<View>
+				<Button title="Sign In WIth Google" onPress={() => this.signInWithGoogleAsync()} />
+			</View>
+		)
+	}
+}
+
+
+export default class App extends React.Component {
+	render () {
+		return (
+			<AppNavigator />
+		)
+	}
+}
---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyABgmi_yFk9GIlXedwW2RRSPv8vzYjWnXs",
+    authDomain: "medixly-d2bbf.firebaseapp.com",
+    projectId: "medixly-d2bbf",
+    storageBucket: "medixly-d2bbf.appspot.com",
+    messagingSenderId: "991102494408",
+    appId: "1:991102494408:web:a7a800e5950f759c7d27fd",
+    measurementId: "G-BF3620PTEM"
+  };
