6b3b422592d1d3da66784252561bdd1d17fb9dae
23yimichael <63271391+23yimichael@users.noreply.github.com>
2021-03-20 13:16:42 -0700

    Update App.js

--- App.js
+++ App.js
@@ -3,13 +3,10 @@ import { createStackNavigator } from "react-navigation-stack";
 import Detect from "./App/Screens/Detect";
 import Results from "./App/Screens/Results";
 import PastResults from "./App/Screens/PastResults";
-import firebase from 'firebase/app';
-import { firebaseConfig } from "./config";
 import React, {Component} from 'react';
 import * as GoogleSignIn from 'expo-google-sign-in';
 import { Button } from "react-native";
 
-firebase.initializeApp(firebaseConfig);
 const navigator = createStackNavigator(
   {
     Detect: Detect,
@@ -23,51 +20,11 @@ const navigator = createStackNavigator(
     },
   }
 );
-const AppNavigator = createAppContainer(navigator);
 
 class LoadingScreen extends Component {
-  signInWithGoogleAsync = async () => {
-    try {
-      const result = await Google.logInAsync({
-        //androidClientId: YOUR_CLIENT_ID_HERE,
-        behavior: "web",
-        iosClientId:
-          "823887287984-b4s258gu72inqefe5mplcessk492364u.apps.googleusercontent.com",
-        scopes: ["profile", "email"],
-      });
-
-      if (result.type === "success") {
-        return result.accessToken;
-      } else {
-        return { cancelled: true };
-      }
-    } catch (e) {
-      return { error: true };
-    }
-  };
-
-  componentDidMount() {
-    this.checkIfLoggedIn();
-  }
-
-  checkIfLoggedIn = () => {
-    firebase.auth().onAuthStateChanged(
-      function (user) {
-        if (user) {
-          this.props.navigation.navigate("Detect");
-        } else {
-          this.props.navigation.navigate("LoginScreen");
-        }
-      }.bind(this)
-    );
-  };
   render() {
     return (
       <View>
-        <Button
-          title="Sign In WIth Google"
-          onPress={() => this.signInWithGoogleAsync()}
-        />
       </View>
     );
   }
@@ -75,6 +32,5 @@ class LoadingScreen extends Component {
 
 export default class App extends React.Component {
   render() {
-    return <AppNavigator />;
   }
 }
