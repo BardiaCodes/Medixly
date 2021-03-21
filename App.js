bd5de709c456c1d198f46127e2f8c2bb488bbe50
AB027897 <47727880+AB027897@users.noreply.github.com>
2021-03-21 14:02:02 -0400

    Fixed Navigation

--- App.js
+++ App.js
@@ -1,36 +1,8 @@
-import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
-import Detect from "./App/Screens/Detect";
-import Results from "./App/Screens/Results";
-import PastResults from "./App/Screens/PastResults";
-import React, {Component} from 'react';
-import * as GoogleSignIn from 'expo-google-sign-in';
-import { Button } from "react-native";
+import * as React from "react";
+import { Text, View } from "react-native";
 
-const navigator = createStackNavigator(
-  {
-    Detect: Detect,
-    Results: Results,
-    PastResults: PastResults,
-  },
-  {
-    initialRouteName: "PastResults",
-    defaultNavigationOptions: {
-      title: "Detection App",
-    },
-  }
-);
+import TabNavigator from "./components/Navigator";
 
-class LoadingScreen extends Component {
-  render() {
-    return (
-      <View>
-      </View>
-    );
-  }
-}
-
-export default class App extends React.Component {
-  render() {
-  }
+export default function App() {
+  return <TabNavigator />;
 }
