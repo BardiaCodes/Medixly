72ca7c2672d34915cb59347ea429f7fa9c389736
Banannabomb <36425629+Banannabomb@users.noreply.github.com>
2021-03-20 22:05:58 -0700

    reorganized some screens, added results and detect to the navigation

--- Navigator.js
+++ Navigator.js
@@ -5,82 +5,82 @@ import { createMaterialBottomTabNavigator } from "react-navigation-material-bott
 import { useNavigation } from "@react-navigation/native";
 import Icon from "react-native-vector-icons/Ionicons";
 
-import HomeScreen from "./HomeScreen";
-import SigninScreen from "./SigninScreen";
-import LoginScreen from "./LoginScreen";
+import HomeScreen from "./App/Screens/HomeScreen";
+import SigninScreen from "./App/Screens/SigninScreen";
+import LoginScreen from "./App/Screens/LoginScreen";
 import ButtonwithBackgroundScreen from "./ButtonWithBackgroundScreen";
 
 const TabNavigator = createMaterialBottomTabNavigator(
-  // Different screen navigations
-  {
-    Home: {
-      screen: HomeScreen,
-      navigationOptions: {
-        tabBarLabel: "Home",
-        tabBarIcon: ({ tintColor }) => (
-          <View>
-            <Icon
-              style={[{ color: tintColor }]}
-              size={25}
-              name={"home-outline"}
-            />
-          </View>
-        ),
-      },
-    },
-    Login: {
-      screen: LoginScreen,
-      navigationOptions: {
-        tabBarLabel: "Login",
-        tabBarIcon: ({ tintColor }) => (
-          <View>
-            <Icon
-              style={[{ color: tintColor }]}
-              size={25}
-              name={"log-in-outline"}
-            />
-          </View>
-        ),
-      },
-    },
-    //    Results: {
-    //      screen: ResultsScreen,
-    //      navigationOptions: {
-    //        tabBarLabel: "Results",
-    //        tabBarIcon: ({ tintColor }) => (
-    //          <View>
-    //            <Icon
-    //              style={[{ color: tintColor }]}
-    //              size={25}
-    //              name={"clipboard"}
-    //            />
-    //          </View>
-    //        ),
-    //      },
-    //    },
-    // Detect: {
-    //   screen: Detect,
-    //   navigationOptions: {
-    //     tabBarLabel: "Detect",
-    //     tabBarIcon: ({ tintColor }) => (
-    //       <View>
-    //         <Icon
-    //           style={[{ color: tintColor }]}
-    //           size={25}
-    //           name={"camera-outline"}
-    //         />
-    //       </View>
-    //     ),
-    //   },
-    // },
-  },
-  // Styles of bottom tab navigator
-  {
-    initialRouteName: "Home",
-    activeColor: "#0000ff",
-    inactiveColor: "#d6efff",
-    barStyle: { backgroundColor: "#42adf5" },
-  }
+	// Different screen navigations
+	{
+		Home: {
+			screen: HomeScreen,
+			navigationOptions: {
+				tabBarLabel: "Home",
+				tabBarIcon: ({ tintColor }) => (
+					<View>
+						<Icon
+							style={[{ color: tintColor }]}
+							size={25}
+							name={"home-outline"}
+						/>
+					</View>
+				),
+			},
+		},
+		Login: {
+			screen: LoginScreen,
+			navigationOptions: {
+				tabBarLabel: "Login",
+				tabBarIcon: ({ tintColor }) => (
+					<View>
+						<Icon
+							style={[{ color: tintColor }]}
+							size={25}
+							name={"log-in-outline"}
+						/>
+					</View>
+				),
+			},
+		},
+		Results: {
+			screen: ResultsScreen,
+			navigationOptions: {
+				tabBarLabel: "Results",
+				tabBarIcon: ({ tintColor }) => (
+					<View>
+						<Icon
+							style={[{ color: tintColor }]}
+							size={25}
+							name={"results-outline"}
+						/>
+					</View>
+				),
+			},
+		},
+		Detect: {
+			screen: Detect,
+			navigationOptions: {
+				tabBarLabel: "Detect",
+				tabBarIcon: ({ tintColor }) => (
+					<View>
+						<Icon
+							style={[{ color: tintColor }]}
+							size={25}
+							name={"camera-outline"}
+						/>
+					</View>
+				),
+			},
+		},
+	},
+	// Styles of bottom tab navigator
+	{
+		initialRouteName: "Home",
+		activeColor: "#0000ff",
+		inactiveColor: "#d6efff",
+		barStyle: { backgroundColor: "#42adf5" },
+	}
 );
 
 export default createAppContainer(TabNavigator);
