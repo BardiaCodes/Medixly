be2db16c60396b9591ede93fe929bd6f4bafc3c1
AB027897 <47727880+AB027897@users.noreply.github.com>
2021-04-15 11:57:50 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { Text, View } from "react-native";
 
-import TabNavigator from "./components/Navigator";
+import TabNavigator from "./Navigator";
 
 export default function App() {
   return <TabNavigator />;
