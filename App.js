e1cb133bbc6fd77ed2a566da477b623db7bdc00a
AB027897 <47727880+AB027897@users.noreply.github.com>
2021-04-15 12:00:08 -0400

    Fixed the file path of the Navigator

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { Text, View } from "react-native";
 
-import TabNavigator from "./Navigator";
+import TabNavigator from "./Navigator.js";
 
 export default function App() {
   return <TabNavigator />;
